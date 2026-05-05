9f5b77e67b1ca31eb432a0b94fee64ff258886b6
mityaw <mitayusuf@student.telkomuniversity.ac.id>
2026-05-05 22:09:23 +0700

    melakukan testing terhadap PBI 37, 38, dan 39

--- POROS/app/Models/Pengumuman.php
+++ POROS/app/Models/Pengumuman.php
@@ -1,15 +1,12 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Pengumuman extends Model
 {
+    use HasFactory;
     protected $table = 'pengumuman';
-
     protected $fillable = ['judul', 'isi', 'user_id'];
-
     public function pembuat()
     {
         return $this->belongsTo(User::class, 'user_id');
